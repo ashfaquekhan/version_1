fdde09e69b7e30452e6c70b3dc783704f8c011d4
Ashfaque Khan <42895491+ashfaquekhan@users.noreply.github.com>
2019-09-18 15:58:00 +0530

    Update showlist.py

--- showlist.py
+++ showlist.py
@@ -106,6 +106,6 @@ while True:
             name="showlist"
             go_src=str(sys.argv[1])
             def_src=str(sys.argv[2])
-            os.system("python pathfinder.py " + str(name) +" "+ str(go_src) +" "+ str(def_src))
+            os.system("python pathfinder.py " + str(name) +" "+ str(go_src) +"/")
 
 
